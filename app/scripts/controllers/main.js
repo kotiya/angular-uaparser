194a5d76be6cba5f2ef2a73cc46e1162e7521056
Weiran Li <jokotiya@gmail.com>
2014-04-18 11:40:13 -0700

    generate project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularUaparserApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
